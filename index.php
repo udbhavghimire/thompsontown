4d25307f49f8fecec79b707fda688068ddb86ca8
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-10-22 22:14:10 +0545

    rounded-pill

--- index.php
+++ index.php
@@ -390,9 +390,8 @@
 
 
 
-    <div class="floating fixcontact"><a href="#Contact"
-            class="btn bgggggggg br-m text-dark w-100 py-1 shadow-lg d-md-none d-block">
-            <span class="d-block text-danger fw-bold">* $75,000 off</span>
+    <div class="floating fixcontact"><a href="#Contact" class="btn bgggggggg rounded-pill  py-1 d-md-none d-block">
+            <span class=" text-white bg-danger px-2 fw-bold">* $75,000 off</span> <br>
             Register Now
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                 class="bi bi-chat-left-dots" viewBox="0 0 16 16">
